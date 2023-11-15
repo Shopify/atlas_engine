05b4b9f8d902016315a7b6e39b73757ff6e8971f
Keith Wiersema <keith.wiersema@shopify.com>
2023-11-15 15:13:51 -0600

    Port loghelper to atlas engine

---
+++ test/helpers/atlas_engine/log_assertion_helper.rb
@@ -0,0 +1,23 @@
+module AtlasEngine
+  module LogAssertionHelper
+    def assert_log_append(level, source, message, additional_params = {})
+      Rails.logger.expects(level).with({
+        messages: [{
+          level: level,
+          source: source,
+          message: message,
+          timestamp: AtlasEngine::LogBase::TEST_TIMESTAMP,
+        }.merge(additional_params)],
+      })
+    end
+
+    def refute_log_append(level, source, message)
+      Rails.logger.expects(level).with(messages: [{
+        level: level,
+        source: source,
+        message: message,
+        timestamp: AtlasEngine::LogBase::TEST_TIMESTAMP,
+      }]).never
+    end
+  end
+end
