daf58d9e4e7ed01599c3117d75e7e223dce8a7ec
Lloyd Saldanha <lloyd.saldanha@shopify.com>
2024-01-03 17:30:19 -0500

    Fixed Missing Files for BE, CH, DK, LI, GB, JP, PL

--- test/models/atlas_engine/country_profile_ingestion_subset_test.rb
+++ test/models/atlas_engine/country_profile_ingestion_subset_test.rb
@@ -6,19 +6,13 @@ require "test_helper"
 module AtlasEngine
   class CountryProfileIngestionSubsetTest < ActiveSupport::TestCase
     test "#open_address_feature_mapper returns the DefaultMapper if no mapper is defined" do
-      # PENDING: enable once all ingestion profiles moved
-      skip
-
       profile = CountryProfile.for("AD")
-      assert_equal "AddressImporter::OpenAddress::DefaultMapper", profile.ingestion.open_address_feature_mapper.class
+      assert_equal AtlasEngine::AddressImporter::OpenAddress::DefaultMapper, profile.ingestion.open_address_feature_mapper
     end
 
-    # test "#open_address_feature_mapper returns the correct feature mapper if defined" do
-    #   # PENDING: enable once all ingestion profiles moved
-    #   skip
-
-    #   profile = CountryProfile.for("AU")
-    #   assert_equal AddressImporter::OpenAddress::Au::Mapper, profile.ingestion.open_address_feature_mapper
-    # end
+    test "#open_address_feature_mapper returns the correct feature mapper if defined" do
+      profile = CountryProfile.for("AU")
+      assert_equal AtlasEngine::Au::AddressImporter::OpenAddress::Mapper, profile.ingestion.open_address_feature_mapper
+    end
   end
 end
