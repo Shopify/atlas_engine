35ecd6a025de291273f60fe876f9f5860f819ac1
Gaby Pancu <gaby.pancu@shopify.com>
2024-01-05 15:28:30 -0500

    add mapper to import TT

---
+++ app/countries/atlas_engine/tt/address_importer/open_address/mapper.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+module AtlasEngine
+  module Tt
+    module AddressImporter
+      module OpenAddress
+        class Mapper < AtlasEngine::AddressImporter::OpenAddress::DefaultMapper
+          sig { params(feature: AtlasEngine::AddressImporter::OpenAddress::Feature).returns(T::Hash[Symbol, T.untyped]) }
+          def map(feature)
+            city, street, number, unit, postcode = feature["properties"].values_at(
+              "city",
+              "street",
+              "number",
+              "unit",
+              "postcode",
+            )
+            {
+              source_id: openaddress_source_id(feature),
+              locale: @locale,
+              country_code: "TT",
+              province_code: nil,
+              city: ["Chaguanas"],
+              suburb: city,
+              zip: postcode,
+              street: street,
+              building_and_unit_ranges: housenumber_and_unit(number, unit),
+              latitude: geometry(feature)&.at(1),
+              longitude: geometry(feature)&.at(0),
+            }
+          end
+        end
+      end
+    end
+  end
+end
